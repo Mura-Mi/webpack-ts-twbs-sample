839a75091701e071feb7fc7a173d02b0d8aa4d2c
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2016-04-08 21:05:49 +0900

    Configure bootstrap variable settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  entry: [ './public/javascripts/common' ],
+  entry: [ 'bootstrap-loader', './public/javascripts/common' ],
   output: {
     filename: '[name].bundle.js',
     path: './public/javascripts/dist'
   },
   resolve: {
-    extensions: [ '', '.ts', '.scss' ]
+    extensions: [ '', '.ts', '.scss', '.js' ]
   },
   module: {
     loaders: [
-      { test: /\.tsx?$/, loader: 'ts-loader' },
+      { test: /\.tsx?$/, loader: 'ts-loader', exclude: /node_modules/ },
       { test: /\.scss$/, loaders: [ 'style', 'css', 'sass' ] },
       { test: /\.woff2?$|\.ttf$|\.eot$|\.svg$/, loader: 'file'}
     ]
